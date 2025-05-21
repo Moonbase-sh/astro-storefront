609fc5e734b07cdad8950c5adc7f5797c35e1c40
Tobias Lønnerød Madsen <m@dsen.tv>
2025-05-21 10:01:38 +0200

    Add GH pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://moonbase-sh.github.io',
+  base: 'astro-storefront',
   vite: {
     plugins: [tailwindcss()]
   }
